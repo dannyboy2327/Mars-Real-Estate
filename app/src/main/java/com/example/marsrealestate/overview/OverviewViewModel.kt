9f2243764f7c99cdff600daf94cc12bfe8cf8ff8
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-02-05 16:41:14 -0800

    feat: Added a retrofit call with retrofit network call to getMarsRealEstateProperties function to access the json and add it to UI controller

--- app/src/main/java/com/example/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/marsrealestate/overview/OverviewViewModel.kt
@@ -3,6 +3,9 @@ package com.example.marsrealestate.overview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.marsrealestate.network.MarsApi
+import retrofit2.Call
+import retrofit2.Response
 
 class OverviewViewModel : ViewModel() {
 
@@ -17,6 +20,15 @@ class OverviewViewModel : ViewModel() {
     }
 
     private fun getMarsRealEstateProperties() {
-        _response.value = "Set the Mars API Response here!"
+        MarsApi.retrofitService.getProperties().enqueue(object : retrofit2.Callback<String> {
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                if (response.isSuccessful) _response.value = response.body()
+            }
+
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                _response.value = "Failure" + t.message
+            }
+
+        })
     }
 }
